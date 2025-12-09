a3711f4a95a2a35df4366c8a6403adb932ccd1db
SERAFINO <ymontenegro@estudiantec.cr>
2025-12-09 17:00:14 -0600

    Hotfix: Codigo comentado

--- lumentech-website/app/page.tsx
+++ lumentech-website/app/page.tsx
@@ -4,14 +4,6 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Navbar />
-      {/* Seccion hero para presentación principal */}
-      {/* <section className="pt-16 min-h-screen bg-gradient-to-br from-blue-600 via-purple-600 to-purple-900">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
-          <h1 className="text-white text-5xl font-bold">
-            Desarrollo de Software de Alto Rendimiento
-          </h1>
-        </div>
-      </section> */}
       <section className="pt-16 min-h-screen bg-indigo-300">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
           <h1 className="text-white text-5xl font-bold">
